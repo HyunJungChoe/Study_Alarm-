f08ba8f9f4643fd7489379c458bc82f537a5e4ce
jerry0121 <40732155+jerry0121@users.noreply.github.com>
2018-07-01 18:12:13 +0900

    Add files via upload

---
+++ 씨샵 기말3 - 복사본 - 복사본/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_min = new System.Windows.Forms.TextBox();
+            this.txt_hour = new System.Windows.Forms.TextBox();
+            this.txt_minutes = new System.Windows.Forms.TextBox();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_setup = new System.Windows.Forms.Button();
+            this.label_alarm = new System.Windows.Forms.Label();
+            this.timer_realtime = new System.Windows.Forms.Timer(this.components);
+            this.timer_setting = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(60, 66);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(80, 18);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "듀오링고";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(60, 140);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(44, 18);
+            this.linkLabel2.TabIndex = 1;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "CNN";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(60, 206);
+            this.linkLabel3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(81, 18);
+            this.linkLabel3.TabIndex = 2;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "coursera";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Location = new System.Drawing.Point(60, 273);
+            this.linkLabel4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(40, 18);
+            this.linkLabel4.TabIndex = 3;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "TED";
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.linkLabel2);
+            this.groupBox1.Controls.Add(this.linkLabel3);
+            this.groupBox1.Controls.Add(this.linkLabel4);
+            this.groupBox1.Location = new System.Drawing.Point(30, 30);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(206, 407);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "즐거운 영어 공부";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 130);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(745, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(417, 407);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "파파고 번역기";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 45);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "번역하기 ↓";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 253);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(370, 130);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.groupBox4);
+            this.groupBox3.Controls.Add(this.txt_minutes);
+            this.groupBox3.Controls.Add(this.btn_cancel);
+            this.groupBox3.Controls.Add(this.btn_setup);
+            this.groupBox3.Controls.Add(this.label_alarm);
+            this.groupBox3.Location = new System.Drawing.Point(292, 30);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(396, 407);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "알림 설정";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "분 후";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.txt_min);
+            this.groupBox4.Controls.Add(this.txt_hour);
+            this.groupBox4.Location = new System.Drawing.Point(57, 157);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(277, 100);
+            this.groupBox4.TabIndex = 16;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "특정 시각(24시)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(233, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 18);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "분 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "시";
+            // 
+            // txt_min
+            // 
+            this.txt_min.Location = new System.Drawing.Point(151, 49);
+            this.txt_min.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_min.Name = "txt_min";
+            this.txt_min.Size = new System.Drawing.Size(75, 28);
+            this.txt_min.TabIndex = 13;
+            this.txt_min.TextChanged += new System.EventHandler(this.txt_min_TextChanged);
+            // 
+            // txt_hour
+            // 
+            this.txt_hour.Location = new System.Drawing.Point(29, 49);
+            this.txt_hour.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_hour.Name = "txt_hour";
+            this.txt_hour.Size = new System.Drawing.Size(75, 28);
+            this.txt_hour.TabIndex = 10;
+            this.txt_hour.TextChanged += new System.EventHandler(this.txt_hour_TextChanged);
+            // 
+            // txt_minutes
+            // 
+            this.txt_minutes.Location = new System.Drawing.Point(132, 97);
+            this.txt_minutes.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_minutes.Name = "txt_minutes";
+            this.txt_minutes.Size = new System.Drawing.Size(75, 28);
+            this.txt_minutes.TabIndex = 15;
+            this.txt_minutes.TextChanged += new System.EventHandler(this.txt_minutes_TextChanged);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(237, 290);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 63);
+            this.btn_cancel.TabIndex = 12;
+            this.btn_cancel.Text = "취소";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // btn_setup
+            // 
+            this.btn_setup.Location = new System.Drawing.Point(86, 290);
+            this.btn_setup.Name = "btn_setup";
+            this.btn_setup.Size = new System.Drawing.Size(75, 63);
+            this.btn_setup.TabIndex = 11;
+            this.btn_setup.Text = "설정";
+            this.btn_setup.UseVisualStyleBackColor = true;
+            this.btn_setup.Click += new System.EventHandler(this.btn_setup_Click);
+            // 
+            // label_alarm
+            // 
+            this.label_alarm.AutoSize = true;
+            this.label_alarm.Location = new System.Drawing.Point(64, 42);
+            this.label_alarm.Name = "label_alarm";
+            this.label_alarm.Size = new System.Drawing.Size(97, 18);
+            this.label_alarm.TabIndex = 11;
+            this.label_alarm.Text = "label_alarm";
+            // 
+            // timer_realtime
+            // 
+            this.timer_realtime.Tick += new System.EventHandler(this.timer_realtime_Tick);
+            // 
+            // timer_setting
+            // 
+            this.timer_setting.Tick += new System.EventHandler(this.timer_setting_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 463);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txt_min;
+        private System.Windows.Forms.TextBox txt_hour;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_setup;
+        private System.Windows.Forms.Label label_alarm;
+        private System.Windows.Forms.Timer timer_realtime;
+        private System.Windows.Forms.Timer timer_setting;
+        private System.Windows.Forms.TextBox txt_minutes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 씨샵 기말3 - 복사본 - 복사본/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using Papago;
+
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        //알람 설정 가능한지 여부
+        bool setup_enable = true;
+
+        //설정할 시간
+        int m_hour = 0;
+        int m_min = 0;
+
+        //DateTime 인터벌 변경
+
+        static readonly int MinTimeInterval = 60 * 1000;
+        static readonly int HourTimeInterval = 60 * 60 * 1000;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //알림 시작할때? tray아이콘 뜨도록
+            NotifyIcon notifycon1 = new NotifyIcon();
+
+            notifyIcon1.Visible = true;
+
+            notifyIcon1.Icon = SystemIcons.Information;
+
+            notifyIcon1.BalloonTipTitle = "영어 공부할 시간 입니다.";
+
+            notifyIcon1.BalloonTipText = "영어.";
+
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+
+            notifyIcon1.ShowBalloonTip(1000);
+
+        }
+
+        // 현재시간 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label_alarm.Text = "현재시간 :" + DateTime.Now.ToString();
+
+            label_alarm.Visible = true;
+
+            timer_realtime.Enabled = true;
+
+
+        }
+
+        // 링크 설정
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.duolingo.com/");
+
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://edition.cnn.com/studentnews/");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.coursera.org/");
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.ted.com/");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //현재 시간 타이머
+        private void timer_realtime_Tick(object sender, EventArgs e)
+        {
+            label_alarm.Text = "현재시간:" + DateTime.Now.ToString();
+        }
+
+        //설정 시간 타이머 
+        private void timer_setting_Tick(object sender, EventArgs e)
+        {
+            if (!setup_enable)
+            {
+                MessageBox.Show("영어 공부할 시간입니다 !!");
+
+                InitAll_Item(); //알람 후 초기화
+            }
+        }
+
+        // notifyIcon 을 더블 클릭 할 때 
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        //설정 버튼 클릭 할 때
+        private void btn_setup_Click(object sender, EventArgs e)
+        {
+            if (setup_enable)
+            {
+                setup_enable = false; //재설정 방지
+
+                CalculateInterval();
+
+                //설정 시간 간격 설정
+                timer_setting.Interval = m_hour * HourTimeInterval + m_min * MinTimeInterval;
+
+                timer_setting.Enabled = true;
+
+            }
+
+            DisableTextBox();
+            btn_cancel.Enabled = true;
+            btn_setup.Enabled = false;
+
+        }
+
+        //취소 버튼 클릭 할 때
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("알람을 취소하겠습니까?");
+            InitAll_Item(); //취소, 초기화
+        }
+
+        // '몇 분후' 텍스트 박스 설정 
+        private void txt_minutes_TextChanged(object sender, EventArgs e)
+        {
+            txt_hour.Enabled = false;
+            txt_min.Enabled = false;
+
+            if (txt_minutes.TextLength == 0)
+            {
+                txt_hour.Enabled = true;   //특정 시각 '시간'
+                txt_min.Enabled = true; //특정 시각 '분'
+            }
+
+
+        }
+
+        //특정시각 '시' 텍스트 박스
+        private void txt_hour_TextChanged(object sender, EventArgs e)
+        {
+            //특정시각 설정시 '몇 분후' 설정 불가
+            txt_minutes.Enabled = false;
+
+            if (txt_hour.TextLength == 0)
+            {
+                txt_minutes.Enabled = true;
+
+            }
+        }
+
+        //특정시각 '분' 텍스트 박스
+        private void txt_min_TextChanged(object sender, EventArgs e)
+        {
+            //특정시각 설정시 '몇 분후' 설정 불가
+            txt_minutes.Enabled = false;
+
+            if (txt_min.TextLength == 0)
+            {
+                txt_minutes.Enabled = true;
+            }
+
+
+
+        }
+
+        //설정 초기화
+        private void InitAll_Item()
+        {
+            setup_enable = true;
+            timer_setting.Interval = 100;
+            m_hour = 0;
+            m_min = 0;
+            txt_minutes.Text = "";
+            txt_hour.Text = "";
+            txt_min.Text = "";
+            txt_hour.Enabled = true;
+            txt_min.Enabled = true;
+            btn_setup.Enabled = true;
+            btn_cancel.Enabled = false;
+
+        }
+
+        //비활성화 하기
+        private void DisableTextBox()
+        {
+            txt_minutes.Enabled = false;
+            txt_hour.Enabled = false;
+            txt_min.Enabled = false;
+
+        }
+
+        //인터벌 계산
+        private void CalculateInterval()
+        {
+            if (txt_minutes.Enabled)
+            {
+                m_hour = Convert.ToInt32(txt_minutes.Text) / 60;
+                m_min = Convert.ToInt32(txt_minutes.Text);
+
+            }
+            else
+            {
+                m_hour = (Convert.ToInt32(txt_hour.Text) - System.DateTime.Now.Hour);
+
+                m_min = Convert.ToInt32(txt_min.Text) - System.DateTime.Now.Minute;
+
+            }
+
+        }
+
+        private void label_alarm_Click(object sender, EventArgs e)
+        {
+            label_alarm.Text = "현재시간 :" + DateTime.Now.ToString();
+
+            label_alarm.Visible = true;
+
+            timer_realtime.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            papago();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void papago()
+
+        {
+            string temp;
+            Translate PapagoTest = new Translate();
+            Translate.NaverClientId = "lo35yY6UeWL7ePyAGX28";
+            Translate.NaverClientSecret = "nmeqoqRucw";
+
+            temp = PapagoTest.NMT("en", "ko", "");
+            Console.WriteLine(temp);
+
+
+
+        }
+    }
+
+
+ }
+
+
